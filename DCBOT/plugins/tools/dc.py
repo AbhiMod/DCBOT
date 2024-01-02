55c82662e3f30655642b1b6931bcfd4076af2980
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2024-01-02 18:11:14 +0530

    Update dc.py

--- DCBOT/plugins/tools/dc.py
+++ DCBOT/plugins/tools/dc.py
@@ -3,6 +3,9 @@ from pyrogram.types import Message
 from DCBOT import app
 from DCBOT.utils.database import get_served_chats
 from config import LOGS
+import random
+from pyrogram.types import(InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, InputMediaVideo, Message)
+
 
 async def lul_message(chat_id: int, message: str):
     await app.send_message(chat_id=chat_id, text=message)
@@ -21,3 +24,14 @@ async def on_new_chat_members(client: Client, message: Message):
             chatusername = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
         lemda_text = f"🌹 ʙᴏᴛ ᴀᴅᴅᴇᴅ ᴛᴏ ɴᴇᴡ ɢʀᴏᴜᴘ ..\n\n┏━━━━━━━━━━━━━━━━━┓\n┣★ **ᴄʜᴀᴛ** › : {matlabi_jhanto}\n┣★ **ᴄʜᴀᴛ ɪᴅ** › : {chat_id}\n┣★ **ᴄʜᴀᴛ ᴜɴᴀᴍᴇ** › : {chatusername}\n┣★ **ᴛᴏᴛᴀʟ ᴄʜᴀᴛ** › : {served_chats}\n┣★ **ᴀᴅᴅᴇᴅ ʙʏ** › :\n┗━━━ {added_by}"
         await lul_message(LOGS, lemda_text)
+        
+@app.on_message(filters.left_chat_member)
+async def on_left_chat_member(_, message: Message):
+    if (await app.get_me()).id == message.left_chat_member.id:
+        remove_by = message.from_user.mention if message.from_user else "𝐔ɴᴋɴᴏᴡɴ 𝐔sᴇʀ"
+        title = message.chat.title
+        username = f"@{message.chat.username}" if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐂ʜᴀᴛ"
+        chat_id = message.chat.id
+        lemda_text = f"ʙᴏᴛ ʟᴇꜰᴛ ᴛᴏ ɢʀᴏᴜᴘ ..\n\n┏━━━━━━━━━━━━━━━━━┓\n┣★ **ᴄʜᴀᴛ** › : {title}\n┣★ **ᴄʜᴀᴛ ɪᴅ** › : {chat_id}\n┣★ **ʙᴏᴛ ᴜɴᴀᴍᴇ** › : {username}\n┣★ **ʀᴇᴍᴏᴠᴇᴅ ʙʏ** › :\n┗━━━ {remove_by}"
+        await app.send_message(LOGS, lemda_text)
+
